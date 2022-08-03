50ba103511ec4f0b9e999314cf5f0cc2734846e8
Jhon Orjuela <tivenorjuela7@gmail.com>
2022-08-03 17:38:55 -0500

    Insertando menu mobile en el navbar

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-const emailNav = document.querySelector('.navbar-email')
+const emailNav = document.querySelector('.navbar-email');
 const menuDesktop = document.querySelector('.inactive');
+const hamMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 emailNav.addEventListener('click', toggleDesktopMenu);
+hamMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu () {
   menuDesktop.classList.toggle('inactive')
+}
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle('inactive');
 }
